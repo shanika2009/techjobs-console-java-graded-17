8df0d8a0d9fbefb0bf1d199da43d5166f3ccd3fb
Shanika <shanika.stovall@gmail.com>
2023-09-04 06:52:41 -0500

    3 out of 4 test passed

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -10,7 +10,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -112,23 +112,30 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        for (HashMap<String, String> jobSearch : someJobs) {
+            System.out.println("\n*****");
 
-//        System.out.println("printJobs is not implemented yet");
-        for (HashMap<String, String> row : someJobs) {
+            for (String job : jobSearch.keySet()) {
 
+                System.out.println(job+ ": " + jobSearch.get(job));
+            }
+            System.out.println("*****");
+
+            if (someJobs.isEmpty()) {
+                System.out.print("No Results");
         }
-
-
-
-
+        }
 
     }
 
+
+
 }
+
